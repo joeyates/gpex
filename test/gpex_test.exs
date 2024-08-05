d51a0989d194bd81232aad0696bbb3a8c7ae0892
Joe Yates <joe.g.yates@gmail.com>
2024-08-05 12:01:03 +0200

    Test for specific parts of the parsed file

--- test/gpex_test.exs
+++ test/gpex_test.exs
@@ -3,28 +3,27 @@ defmodule GpexTest do
   alias Gpex
 
   describe "parse/1" do
-    test "parses" do
+    test "parses tracks" do
       {:ok, gpx_data} = File.read("test/fixtures/minimal.gpx")
 
-      assert Gpex.parse(gpx_data) ==
-               %Gpex{
-                 tracks: [
-                   %Gpex.Track{
-                     segments: [
-                       %Gpex.TrackSegment{
-                         points: [
-                           %Gpex.Point{
-                             longitude: 11.47096552,
-                             latitude: 43.74124841,
-                             elevation: 334.29998779296875,
-                             time: ~U[2015-09-19 08:07:38Z]
-                           }
-                         ]
-                       }
-                     ]
-                   }
-                 ]
-               }
+      parsed = Gpex.parse(gpx_data)
+
+      point =
+        get_in(parsed, [
+          Access.key(:tracks),
+          Access.at(0),
+          Access.key(:segments),
+          Access.at(0),
+          Access.key(:points),
+          Access.at(0)
+        ])
+
+      assert point == %Gpex.Point{
+               longitude: 11.47096552,
+               latitude: 43.74124841,
+               elevation: 334.29998779296875,
+               time: ~U[2015-09-19 08:07:38Z]
+             }
     end
   end
 
